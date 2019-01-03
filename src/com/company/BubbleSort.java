fdd806dc6553d98f591bb406423180160c235e28
Vincent Tran <vtran6971@bths.edu>
2019-01-02 19:03:55 -0500

    Working but not efficient

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -3,25 +3,37 @@ package com.company;
 public class BubbleSort {
     public static void bubbleSort(int[] arr)
     {
-        int count = 0;
+        int count = 1;
         int hold;
         int i = 0;
         int j = 1;
 
-        while(count < arr.length)
-        {
-            if (arr[i] < arr[j])
-            {
+        while (count > 0) {
+            if (arr[i] > arr[j]) {
                 hold = arr[j];
                 arr[j] = arr[i];
                 arr[i] = hold;
+                i++;
+                j++;
+                count++;
             }
-            i++;
-            j++;
-            if(j== arr.length)
+            else
             {
-                count=arr.length;
+                i++;
+                j++;
+            }
+
+            if(j == arr.length && count > 1)
+            {
+                i = 0;
+                j = 1;
+                count = 1;
+            }
+            if(j == arr.length && count == 1)
+            {
+                count = 0;
             }
         }
-    }
+        }
+
 }
