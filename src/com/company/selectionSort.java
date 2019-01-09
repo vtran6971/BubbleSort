03de24327c4aae2012c15af5a8cf2a02d26f0dc8
Vincent Tran <vtran6971@bths.edu>
2019-01-08 22:10:28 -0500

    SelectionSort completed and commented

--- src/com/company/selectionSort.java
+++ src/com/company/selectionSort.java
@@ -3,28 +3,31 @@ package com.company;
 public class selectionSort {
     public static void SelectionSort(int[] array)
     {
-      int indexofMin = 0;
-      int i = 0;
-      int hold = 0;
+     int min = 1001;
+     int indexofmin = 0;
+     int hold = 0;
 
-            for (int j = i; j < array.length; j++)
-            {
-                if(array[j] < array[j+1])
-                {
-                  indexofMin = j;
-                }
-                if(j == array.length-2)
-                {
-                    hold = array[i];
-                 array[i] = array[indexofMin];
-                 array[indexofMin] = hold;
-                 i++;
-                 j = i;
-                }
-                if(i == array.length-1)
-                {
-                    j = array.length;
-                }
-            }
+     //First for loop is used to increment the index at which we're checking minimums
+      for(int i = 0; i < array.length; i++)
+      {
+          //Second for loop used to search through the whole array for minimums
+          for (int j = i; j < array.length; j++)
+          {
+              //if a minimum is found store the value and index
+              if(array[j] < min)
+              {
+                  min = array[j];
+                  indexofmin = j;
+              }
+              //once we reached the end of array place the minimum at index i (i will have increased by one every time we do this)
+              if (j == array.length-1)
+              {
+                  hold = array[i];
+                  array[i] = min;
+                  array[indexofmin] = hold;
+                  min = 1001;
+              }
+          }
+      }
     }
 }
